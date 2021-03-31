2848efb899f24607217b976fdcc808372e89f6be
= <=>
2021-03-31 11:28:20 +0100

    Initial puppeteer setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.bbc.co.uk');
+  await page.screenshot({ path: 'bcc-news.png' });
+
+  await browser.close();
+})();
