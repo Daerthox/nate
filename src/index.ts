ca3217f67e355904ccd7316a7a66784efb92260b
= <=>
2021-04-02 21:13:25 +0100

    abstract tagging away

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer'
+import { tagElements, tagElementsWithContent } from './tagging'
 import { createWriteStream, mkdirSync, existsSync } from 'fs'
 
 const navigate = async (url: string) => {
@@ -12,14 +13,7 @@ const navigate = async (url: string) => {
   await page.goto(url)
 
   let selector = 'input[type="button"][value="Start"]'
-  await page.evaluate((selector) => {
-    const elem = document.querySelector(selector)
-    if (!elem) {
-      throw new Error(`could not locate element: ${selector}`)
-    }
-    elem.setAttribute('nate-action-type', 'click')
-  }, selector)
-
+  await tagElements(page, selector, { 'nate-action-type': undefined })
   let content = await page.content()
 
   const [page2] = await Promise.all([
@@ -43,13 +37,7 @@ const navigate = async (url: string) => {
   })
 
   selector = '.custom-select-trigger'
-  await page.evaluate((selector) => {
-    const elem = document.querySelector(selector)
-    if (!elem) {
-      throw new Error(`could not locate element: ${selector}`)
-    }
-    elem.setAttribute('nate-action-type', 'click')
-  }, selector)
+  await tagElements(page, selector, { 'nate-action-type': 'click' })
 
   content = await page.content()
   page.click(selector)
@@ -59,6 +47,7 @@ const navigate = async (url: string) => {
   writer.close()
 
   selector = '//span[contains(., "London")]'
+  await tagElementsWithContent(page, 'span', { 'nate-action-type': 'click' }, 'London')
   const city = await page.waitForXPath(selector, { visible: true })
 
   if (!city) {
@@ -69,13 +58,7 @@ const navigate = async (url: string) => {
   await city.click({ delay: 100 })
 
   selector = '#next-page-btn:enabled'
-  await page.evaluate((selector) => {
-    const elem = document.querySelector(selector)
-    if (!elem) {
-      throw new Error(`could not locate element: ${selector}`)
-    }
-    elem.setAttribute('nate-action-type', 'click')
-  }, selector)
+  await tagElements(page, selector, { 'nate-action-type': 'click' })
 
   content = await page.content()
 
@@ -102,13 +85,7 @@ const navigate = async (url: string) => {
   try {
     await page.waitForSelector('#popup', { visible: true, timeout: 10000 })
     selector = 'input[type="button"][value="X"]'
-    await page.evaluate((selector) => {
-      const elem = document.querySelector(selector)
-      if (!elem) {
-        throw new Error(`could not locate element: ${selector}`)
-      }
-      elem.setAttribute('nate-action-type', 'click')
-    }, selector)
+    await tagElements(page, selector, { 'nate-action-type': 'click' })
     content = await page.content()
     const close = await page.waitForSelector(selector)
     close && close.click()
@@ -116,44 +93,56 @@ const navigate = async (url: string) => {
     writer.write(content)
     writer.close()
   } catch {
-    console.log('no popup')
+    // probably no popup
+    console.log('maybe no popup')
   }
 
+  await tagElements(page, '#name', { 'nate-action-type': 'input', 'nate-dict-key': 'nate' })
   const nameInput = await page.waitForSelector('#name', { visible: true })
   nameInput && (await nameInput.type('nate', { delay: 300 }))
 
+  await tagElements(page, '#pwd', { 'nate-action-type': 'input', 'nate-dict-key': '07000000000' })
   const pwdInput = await page.waitForSelector('#pwd', { visible: true })
   pwdInput && (await pwdInput.type('07000000000', { delay: 300 }))
 
+  await tagElements(page, '#phone', { 'nate-action-type': 'input', 'nate-dict-key': '07000000000' })
   const phoneInput = await page.waitForSelector('#phone', { visible: true })
   phoneInput && (await phoneInput.type('07000000000', { delay: 300 }))
 
+  await tagElements(page, '#email', { 'nate-action-type': 'input', 'nate-dict-key': 'nate@nate.tech' })
   const emailInput = await page.waitForSelector('#email', { visible: true })
   emailInput && (await emailInput.type('nate@nate.tech', { delay: 300 }))
 
-  const genderCheckbox = await page.waitForSelector(
-    'input[type="checkbox"][value="female"]'
-  )
+  selector = 'input[type="checkbox"][value="female"]'
+  await tagElements(page, selector, { 'nate-action-type': 'check' })
+  const genderCheckbox = await page.waitForSelector(selector)
   genderCheckbox && (await genderCheckbox.click())
 
+  await tagElements(page, '#btn', { 'nate-action-type': 'click' })
   const submit = await page.waitForSelector('#btn', { visible: true })
 
-  if (!submit) {
-    return null
-  }
+  content = await page.content()
+
+  writer = createWriteStream(`${logsDir}/${Date.now()}.html`)
+  writer.write(content)
+  writer.close()
 
   const [page4] = await Promise.all([
     page.waitForNavigation({ timeout: 5000 }),
-    submit.click(),
+    submit && submit.click(),
   ])
 
   if (!page4 || !page4.ok()) {
     return null
   }
 
-  await page.screenshot({ path: 'screenshot.png' })
-  await browser.close()
+  content = await page.content()
+
+  writer = createWriteStream(`${logsDir}/${Date.now()}.html`)
+  writer.write(content)
   writer.close()
+
+  await browser.close()
 }
 
 navigate(
