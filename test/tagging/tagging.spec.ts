ce83724461a97e8270ce137d3aecf941a3b96a74
= <=>
2021-04-06 14:52:23 +0100

    initial test setup

---
+++ test/tagging/tagging.spec.ts
@@ -0,0 +1,26 @@
+import { tagElement, tagElementWithContent } from '../../src/tagging'
+
+describe('tagging API', () => {
+    describe('tagElement()', () => {
+        it('should throw an error when no element matches the selector', () => {
+            throw Error('not implemented')
+        })
+        it('should throw an error when more then one element matches the selector', () => {
+            throw Error('not implemented')
+        })
+        it('should tag the element targeted by the selector', () => {
+            throw Error('not implemented')
+        })
+    })
+    describe('tagElementWithContent()', () => {
+        it('should throw an error when no element with the specified content matches the selector', () => {
+            throw Error('not implemented')
+        })
+        it('should throw an error when more then one element with the specified content matches the selector', () => {
+            throw Error('not implemented')
+        })
+        it('should tag the element targeted by the selector if it has the specified content', () => {
+            throw Error('not implemented')
+        })
+    })
+})
