372e5a8f227b3f6bcd3bd16d406c1c29fb721228
= <=>
2021-04-07 12:35:43 +0100

    linting

--- test/logger/logger.spec.ts
+++ test/logger/logger.spec.ts
@@ -3,22 +3,23 @@ import { readFile, rm } from 'fs/promises'
 import { createLogger } from '../../src/logger'
 
 describe('logging API', () => {
-    const html = '<!DOCTYPE html>' +
-        '<html>' +
-        '<head>' +
-        '<title>Initial page</title>' +
-        '<style>' +
-        'div {' +
-        'color: blue;' +
-        '}' +
-        '</style>' +
-        '</head>' +
-        '<body>' +
-        '<div id="original">blue</div>' +
-        '</body>' +
-        '</html>'
+  const html =
+    '<!DOCTYPE html>' +
+    '<html>' +
+    '<head>' +
+    '<title>Initial page</title>' +
+    '<style>' +
+    'div {' +
+    'color: blue;' +
+    '}' +
+    '</style>' +
+    '</head>' +
+    '<body>' +
+    '<div id="original">blue</div>' +
+    '</body>' +
+    '</html>'
 
-    const modified = (`
+  const modified = `
         <!DOCTYPE html>
         <html>
             <head>
@@ -33,59 +34,63 @@ describe('logging API', () => {
                 <div id="modified">red</div>
             </body>
         </html>
-    `)
+    `
 
-    type setupValues = [Page, Browser]
-    const setupPage = async () => {
-        const browser = await puppeteer.launch()
-        const page = await browser.newPage()
-        await page.setContent(html)
+  type setupValues = [Page, Browser]
+  const setupPage = async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setContent(html)
 
-        return [page, browser] as setupValues
-    }
+    return [page, browser] as setupValues
+  }
 
-    const rootLogDir = `${__dirname}/logs`
+  const rootLogDir = `${__dirname}/logs`
 
-    describe('page logger', () => {
-        it('should log the page html and css to a files in the specified directory', async () => {
-            const browser = await puppeteer.launch()
-            const page = await browser.newPage()
-            await page.setContent(html)
+  describe('page logger', () => {
+    it('should log the page html and css to a files in the specified directory', async () => {
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.setContent(html)
 
-            const dir = `${rootLogDir}/logs`
+      const dir = `${rootLogDir}/logs`
 
-            const logger = await createLogger(page, dir)
-            
-            // Test logPage()
-            let filename = 'initial'
-            await logger.logPage(filename)
+      const logger = await createLogger(page, dir)
 
-            let content = await readFile(`${dir}/${filename}.html`, { encoding: 'utf-8' })
-            expect(content).toBe(html)
+      // Test logPage()
+      let filename = 'initial'
+      await logger.logPage(filename)
 
-            let css = await readFile(`${dir}/${filename}-0.css`, { encoding: 'utf-8' })
-            expect(css).toBe('div { color: blue; }\n')
+      let content = await readFile(`${dir}/${filename}.html`, {
+        encoding: 'utf-8',
+      })
+      expect(content).toBe(html)
 
-            // Test snapshot()
-            filename = 'fromSnapshot'
-            const snapshot = await logger.pageSnapshot()
+      let css = await readFile(`${dir}/${filename}-0.css`, {
+        encoding: 'utf-8',
+      })
+      expect(css).toBe('div { color: blue; }\n')
 
-            await page.setContent(modified)
+      // Test snapshot()
+      filename = 'fromSnapshot'
+      const snapshot = await logger.pageSnapshot()
 
-            // confirm the content has changed
-            const div = page.evaluate(() => document.getElementById('modified'))
-            expect(div).not.toBeNull()
+      await page.setContent(modified)
 
-            await snapshot.logPage(filename)
+      // confirm the content has changed
+      const div = page.evaluate(() => document.getElementById('modified'))
+      expect(div).not.toBeNull()
 
-            content = await readFile(`${dir}/${filename}.html`, { encoding: 'utf-8' })
-            expect(content).toBe(html)
+      await snapshot.logPage(filename)
 
-            css = await readFile(`${dir}/${filename}-0.css`, { encoding: 'utf-8' })
-            expect(css).toBe('div { color: blue; }\n')
+      content = await readFile(`${dir}/${filename}.html`, { encoding: 'utf-8' })
+      expect(content).toBe(html)
 
-            await rm(dir, { recursive: true })
-            await browser.close()
-        })
+      css = await readFile(`${dir}/${filename}-0.css`, { encoding: 'utf-8' })
+      expect(css).toBe('div { color: blue; }\n')
+
+      await rm(dir, { recursive: true })
+      await browser.close()
     })
+  })
 })
